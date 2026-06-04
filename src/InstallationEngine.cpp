ad94579c450682de0b96a936bae9cea459f0ef86
Ali Rouhi <thealirouhi@gmail.com>
2026-06-04 18:34:45 +0330

    feat(core): implement addModule and addPackage methods in InstallationEngine

--- src/InstallationEngine.cpp
+++ src/InstallationEngine.cpp
@@ -4,3 +4,22 @@ InstallationEngine::InstallationEngine()
 {
     // nothing required for now
 }
+
+void InstallationEngine::addModule(const std::string& id, const std::string& title)
+{
+    Installable* m = new Module(id, title);
+
+    m->addObserver(&logger);
+
+    allComponents.push_back(m);
+}
+
+void InstallationEngine::addPackage(const std::string& id, const std::string& title)
+{
+    Installable* p = new Package(id, title);
+
+    p->addObserver(&logger);
+
+    allComponents.push_back(p);
+}
+
