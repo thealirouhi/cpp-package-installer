5f60d597f44c82fbff6d414dcefdc617a418736d
Ali Rouhi <thealirouhi@gmail.com>
2026-06-04 16:35:12 +0330

    abstractionfeat(core): add Package header for module container

---
+++ include/Package.hpp
@@ -0,0 +1,28 @@
+#ifndef PACKAGE_HPP
+#define PACKAGE_HPP
+
+#include <vector>
+
+#include "Installable.hpp"
+
+class Package : public Installable
+{
+private:
+    std::vector<Installable*> children;
+
+public:
+    Package(
+        const std::string& id,
+        const std::string& title
+    );
+
+    bool isPackage() const override;
+
+    void addChild(Installable* child);
+
+    bool install(TransactionContext& tx) override;
+
+    void uninstall() override;
+};
+
+#endif
